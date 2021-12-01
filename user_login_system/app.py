55244264f882923fad9258481cdba6c310763356
iammix <k.mixios@gmail.com>
2021-12-01 07:26:09 -0800

    UPDATE

--- user_login_system/app.py
+++ user_login_system/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import pymongo
 
 app = Flask(__name__)
 
+# Database
+client = pymongo.MongoClient('localhost',27017)
+db = client.user_login_system
+
+#Routes
+from user import routes
+
 @app.route('/')
 def home():
-    return "Home"
+    return render_template('home.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
