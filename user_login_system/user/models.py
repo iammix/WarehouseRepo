54925487609fcb500cac26ffa93683e70da7c0d6
iammix <k.mixios@gmail.com>
2021-12-07 16:11:01 +0200

    UPDATE

--- user_login_system/user/models.py
+++ user_login_system/user/models.py
@@ -4,24 +4,19 @@ from pymongo import collation
 from app import mymongodb
 import uuid
 
+
 class User:
     def signup(self):
-
         # Create the user object
         user = {
-            "_id":uuid.uuid4().hex,
-            "name":request.form.get('name'),
-            "email":request.form.get('email'),
-            "password":request.form.get('password')
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "email": request.form.get('email'),
+            "password": request.form.get('password')
         }
 
         # Encrypt the password
         user['password'] = pbkdf2_sha256.encrypt(user['password'])
         collection = mymongodb['users']
         collection.insert_one(user)
-
-
-
-
         return jsonify(user), 200
-    
